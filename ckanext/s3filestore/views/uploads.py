b3fde4cf5a4cc301b0fba592235f09b7e0716d36
saj-derilinx <lakshitha@derilinx.com>
2024-04-09 15:31:25 +0000

    Remove previous signing URL rule

--- ckanext/s3filestore/views/uploads.py
+++ ckanext/s3filestore/views/uploads.py
@@ -32,23 +32,6 @@ s3_uploads = Blueprint(
 )
 
 
-def uploaded_file_redirect(upload_to, filename):
-    '''Redirect static file requests to their location on S3.'''
-
-    storage_path = S3Uploader.get_storage_path(upload_to)
-    filepath = os.path.join(storage_path, filename)
-    base_uploader = BaseS3Uploader()
-
-    try:
-        url = base_uploader.get_signed_url_to_key(filepath)
-    except ClientError as ex:
-        if ex.response['Error']['Code'] in ['NoSuchKey', '404']:
-            return abort(404, _('Keys not found on S3'))
-        else:
-            raise ex
-
-    return redirect(url)
-
 def authorized_to_signv4_upload(resource_id):
     ''' If user is authed to use resource_create then they are
         authued to use signv4_upload()
@@ -116,8 +99,6 @@ def signv4_upload():
     return r
 
 
-s3_uploads.add_url_rule(u'/uploads/<upload_to>/<filename>',
-                        view_func=uploaded_file_redirect)
 s3_uploads.add_url_rule(u'/auth/signv4_upload',
                         view_func=signv4_upload)
 
